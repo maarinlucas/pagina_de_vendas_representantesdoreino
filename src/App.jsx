560f9cfba66c8fc34d5e2f35f50121dae19ca41e
Lucas Marin <josemarin.nj@gmail.com>
2024-12-31 15:52:49 -0300

    'a'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,116 @@
-/* import { useState } from 'react' */
+import { useLayoutEffect } from 'react'
 import './index.css'
 import mockupImage from './assets/mockup.png'
-import logo from './assets/logo.jpg'
+import logo from './assets/logo.png'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 function App() {
   const redirectToKirvano = () => {
     window.location.href = 'https://pay.kirvano.com/776a100e-d53e-4f5e-9a5a-f5ca18bfe53d';
   };
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+      gsap.to(".parte1a", {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".parte1a",
+          start: "top 100%"
+        }
+      });
+
+     
+        gsap.to(".parte1b", {
+          x: 0,
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".parte1a",
+            start: "top 100%"
+          }
+        });
+  
+        gsap.to(".parte2a", {
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".parte2",
+            start: "top 80%"
+          }
+        });
+  
+        gsap.to(".parte2b", {
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".parte2",
+            start: "top 80%"
+          }
+        });
+  
+        gsap.to(".desconto", {
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".parte3",
+            start: "top 80%"
+          }
+        });
+  
+        gsap.to(".h24", {
+          x: 0,
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".parte3",
+            start: "top 80%"
+          }
+        });
+  
+        gsap.to(".chamada", {
+          x: 0,
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".parte4",
+            start: "top 80%"
+          }
+        });
+  
+        gsap.to(".Copyright", {
+          x: 0,
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".parte4",
+            start: "top 80%"
+          }
+        });
+      
+    
+ 
+
+      // Reavaliar quando a janela for redimensionada
+    
+      
+    return () => {
+      gsap.killTweensOf(".parte1a")
+      gsap.killTweensOf(".parte1b")
+      gsap.killTweensOf(".parte2a")
+      gsap.killTweensOf(".parte2b")
+      gsap.killTweensOf(".desconto")
+      gsap.killTweensOf(".chamada")
+      gsap.killTweensOf(".h24")
+      gsap.killTweensOf(".Copyright")
+    }
+    
+
+
+  }, [])
+
 
   return (
     <div className='app'>
@@ -25,11 +129,9 @@ function App() {
 
       <div className="parte2">
         <div className="parte2a">
-          <p>Descubra histórias de grandes heróis da fé que moldaram o cristianismo e marcaram gerações. Este eBook traz um retrato cativante de líderes espirituais como Billy Graham, Charles Spurgeon, John Piper e C.S. Lewis.
-            Com biografias breves e frases impactantes, veja os desafios que superaram, os caminhos
+          <p>Este e-book é uma obra original dos Representantes Do Reino, um canal de comunicação dedicada a pregar o evangelho de forma divertida, descontraída e original, nele você conhecer histórias de ums dos maiores heróis do cristianismo, depois dos apóstolos e profetas, além de poder connhecer algumas de suas frases mais impactantes, antes que se pergunte, este ebook vai te trazer grandes reflexões baseadas
           </p>
-          <p>que seguiram e as verdades que proclamaram com coragem. Mais que celebrar o legado desses gigantes, este livro oferece inspiração para fortalecer sua própria caminhada espiritual.
-            Heróis da Fé não é só história; é um convite para viver com propósito e deixar um impacto duradouro.
+          <p>em um contexto sólido da palavra de Deus, caso você tenha uma certa dificuldade em entende-la em sua totalidade, com certeza este livro te ajudará a entender pelo meno a base do que significa ser um discípulo de Jesus e a importancia de viver segundo os princípios ensinadas pela bíblia, tanto por ele como pelos profetas, adquira o seu e-book clicando no botão abaixo, tenho certeza que a sua leitura irá mudar a sua vida.
           </p>
         </div>
         <div className="parte2b">
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pagina_de_vendas_representantesdoreino/',
   build: {
     outDir: 'dist', // pasta onde o build será gerado
     sourcemap: true, // Gera mapa de fontes, útil para debug
